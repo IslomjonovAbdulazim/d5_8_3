49f8e412fa99c0bcf2d0304882134f2ca4ad5977
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-10 18:26:02 +0500

    imp

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 
@@ -11,9 +12,11 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   AudioPlayer player = AudioPlayer();
   int speed = 1;
+  double volume = 0.5;
 
   @override
   void initState() {
+    player.setAsset("assets/happy_nation.mp3");
     super.initState();
   }
 
@@ -22,13 +25,17 @@ class _HomePageState extends State<HomePage> {
     super.dispose();
   }
 
-  void play() async {}
+  void play() async {
+    player.play();
+  }
 
   void pause() async {}
 
-  void doubleSpeed() async {}
+  void changeSpeed() async {}
 
   void stop() async {}
+
+  void changeVolume() async {}
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +44,62 @@ class _HomePageState extends State<HomePage> {
         child: Padding(
           padding: EdgeInsets.all(20),
           child: Column(
-            children: [],
+            children: [
+              Container(
+                height: 70,
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                  color: Colors.yellow,
+                  border: Border.all(
+                    color: Colors.red,
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(
+                  child: Row(
+                    children: [
+                      Text(
+                        "Music #1",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Spacer(),
+                      CupertinoButton(
+                        padding: EdgeInsets.zero,
+                        onPressed: () {
+                          play();
+                        },
+                        child: Icon(CupertinoIcons.play),
+                      ),
+                      CupertinoButton(
+                        padding: EdgeInsets.zero,
+                        onPressed: () {
+                          stop();
+                        },
+                        child: Icon(CupertinoIcons.stop),
+                      ),
+                      CupertinoButton(
+                        padding: EdgeInsets.zero,
+                        onPressed: () {
+                          changeSpeed();
+                        },
+                        child: Text("${speed == 1 ? 2 : 1}X"),
+                      ),
+                      CupertinoButton(
+                        padding: EdgeInsets.zero,
+                        onPressed: () {
+                          changeVolume();
+                        },
+                        child: Icon(CupertinoIcons.volume_down),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
